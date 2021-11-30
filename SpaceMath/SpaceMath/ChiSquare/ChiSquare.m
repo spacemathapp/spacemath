219a34d75f0558f18def177a68e8afc17f408eeb
tvalencia <53123066+tvalencia@users.noreply.github.com>
2021-11-30 14:00:29 -0600

    New two commands: Chi2Rx68 and Chi2Rx95.

--- SpaceMath/SpaceMath/ChiSquare/ChiSquare.m
+++ SpaceMath/SpaceMath/ChiSquare/ChiSquare.m
@@ -4,8 +4,8 @@
 
 
 Chi2Rx::usage="Chi Square"
-
-
+Chi2Rx95::usage="Chi2Rx95"
+Chi2Rx68::usage="Chi2Rx68"
 (************************************************************************************************************************************************************************************************************************************************************************************************************************)
 
 Begin["`Package`"]
@@ -27,4 +27,35 @@ Chi2Rx[ghtt_, ghbb_, ghtautau_, ghZZ_, ghWW_, gCH_,
   StandardDeviation[{Rbb, Rtautau, Rzz, Rww, Rgammagamma}])  
 
 (************************************************************************************************************************************************************************************************************************************************************************************************************************)
+
+Chi2Rx95[ghtt_, ghbb_, ghtautau_, ghZZ_, ghWW_, gCH_,   mCH_] := 
+ RegionPlot[
+  Chi2Rx[ghtt, ghbb, ghtautau, ghZZ, ghWW, 0, 2000] < 
+   11.070, {Cab, -1, 1}, {tb, 0.1, 50}(*,PlotPoints\[Rule]500*), 
+  PlotStyle -> Blue, Frame -> True, 
+   FrameLabel -> {Style["cos(\[Beta]-\[Alpha])", Larger, Bold], 
+       Style["tan\[Beta]", Larger, Bold]}, 
+   FrameStyle -> Thickness[0.003], 
+   AxesLabel -> {Style["x", Larger, Bold, Bold], 
+       Style["y", Larger, Bold, Bold]}, LabelStyle -> 30, 
+  ImageSize -> 600, PlotLegends -> Placed[{"95% C.L."}, {0.2, 0.1}], 
+  BoundaryStyle -> Blue, PlotPoints -> 100]
+  
+(**********************************************************************************************************************************************************************************************************************************************************************************************************************)  
+
+Chi2Rx68[ghtt_, ghbb_, ghtautau_, ghZZ_, ghWW_, gCH_,   mCH_] := 
+ RegionPlot[
+  Chi2Rx[ghtt, ghbb, ghtautau, ghZZ, ghWW, 0, 2000] < 3.53, {Cab, -1, 
+   1}, {tb, 0.1, 50}, PlotPoints -> 100, PlotStyle -> Green, 
+  Frame -> True, 
+   FrameLabel -> {Style["cos(\[Beta]-\[Alpha])", Larger, Bold], 
+       Style["tan\[Beta]", Larger, Bold]}, 
+   FrameStyle -> Thickness[0.003], 
+   AxesLabel -> {Style["x", Larger, Bold, Bold], 
+       Style["y", Larger, Bold, Bold]}, LabelStyle -> 30, 
+  ImageSize -> 600, 
+  PlotLegends -> Placed[{"68% C.L."(*1\[Sigma]*)}, {0.2, 0.2}], 
+  BoundaryStyle -> Green]
+
+(**********************************************************************************************************************************************************************************************************************************************************************************************************************)  
 End[]
