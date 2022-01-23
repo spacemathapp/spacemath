eb161a5f8f7ef1c3af0967e7f7e41c34212102ce
.. <..>
2022-01-22 19:19:54 -0600

    update

--- SpaceMath/Install.m
+++ SpaceMath/Install.m
@@ -46,7 +46,7 @@ InstallSpaceMath[]:=
 				  ]
 			  ];
 
-			TempCompressFolder = URLSave["https://github.com/spacemathapp/spacemath/archive/refs/heads/stable.zip",CreateTemporary[]];
+			TempCompressFolder = URLSave["https://github.com/spacemathapp/spacemath/archive/refs/heads/development.zip",CreateTemporary[]];
 			DecompressTempFolder = TempCompressFolder<>".dir";
 
 			ExtractArchive[TempCompressFolder, DecompressTempFolder];
